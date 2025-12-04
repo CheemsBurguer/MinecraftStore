8fcec13c6a6b08e2daf82396b57e8f569b3d531b
CheemsBurguer <strangercheems@gmail.com>
2025-12-04 14:32:35 -0600

    Inicializacion de proyecto (ya estaba avanzado antes de hacer el repo)

---
+++ category/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+
+    category_name = models.CharField(max_length=20, unique=True)
+    category_description = models.CharField(max_length=225, blank=True)
+    category_slug = models.CharField(max_length=100, unique=True)
+    category_image = models.ImageField(upload_to= 'photos/category', blank=True)
+
+    class Meta():
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
